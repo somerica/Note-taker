1ef6e7eacf47bf818312c43dd14fe9fba1ecac91
Somer Jackson <somer.jackson@gmail.com>
2021-02-01 20:50:43 -0500

    package json added . Js server opened

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require ("express");
+var path = require ("path");
+var fs = require("fs");
+const { urlencoded } = require("express");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+app.listen(PORT,function(){
+
+});
+
+console.log(PORT)
+
+var savedNotes = JSON.parse(fs.readFileSync(__dirname+'/db/db.json'));
+
+app.get("/", function(req, res) {
+    console.log(__dirname)
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+  
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+  
+  
+  
+app.get("/api/notes", function(req, res) {
+    res.json(savedNotes);
+});
